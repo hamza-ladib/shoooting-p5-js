c4f1c3ae4395a6b72bd5dc84436325d467bd631a
hamza-ladib <109552817+hamza-ladib@users.noreply.github.com>
2022-08-23 15:10:29 +0100

    commit

--- sketch.js
+++ sketch.js
@@ -15,14 +15,21 @@ let hittedArrows=[];
 let player;
 let wall=20;
 let ph=60;
-let n=10;
-
+let n=0;
+let n1=0;
+let n2=0;
+let dir=1;
+let score=0;
+let  moves=20;
+let maxscore=10;
 
 function setup() {
   canvas=createCanvas(width,height);
   canvas.id("cnv");
   /// create targets//
-  for(let i=0;i<5 ;i++){
+  alert("try to hit all balls with arrows using space bar");
+  ////////////////////
+  for(let i=0;i<maxscore ;i++){
      target =new Target(random(2*PI),px,py,r);
     targets.push(target);
     
@@ -37,6 +44,11 @@ background(0);
 /**** CREATE WALL***/
 fill("#3E2723");
 rect (0,0,width,wall);
+/////////////////////////
+textSize(35);
+fill(0, 102, 153);
+text ("score : "+score,10,height-10);
+text ("moves : "+moves,1.9*width/3,height-10);
 /////////////////////
   stroke("grey");
   strokeWeight(pointSize);
@@ -50,12 +62,25 @@ rect (0,0,width,wall);
     else {
       targets[i].isHit=true;
       hittedTarget.push(targets[i]);
-      player.x=player.x-n;
-      n +=40;
+       n=(dir==1)?n1:n2;
+      player.x=player.x-n; 
+      if(dir==1){
+         n1+=40;
+         dir=-1;}
+      else{
+        n2-=40;
+         dir=1;
+      }
+    
+
+
+     
+
       
       hittedArrows.push(player);
       targets.splice(i,1);
       player=new Player(width/2,height-ph);
+      score++;
      
     }
     
@@ -74,15 +99,28 @@ rect (0,0,width,wall);
   if(player.y<-player.h*2){
     player=new Player(width/2,height-ph);
   }
+  
 
   player.show();
+  if(moves<=0){
+    alert("you lost");
+    noLoop();
+ }
+
+  if(score>=maxscore-1){
+     alert("you win");
+     noLoop();
+  }
+
  
   if(frameCount%300==0){
   random(targets).direction*=-1;
   }
+
 }
 function keyPressed(){
    if(keyCode==32){
     player.go=true;
+    moves--;
    }
 }
